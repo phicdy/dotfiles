ef89ffd8f744c1deca079f3f1f58e9e2ce531167
phicdy <phicdy@gmail.com>
2016-06-26 16:58:28 +0900

    Fix temp shell file name

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 # Install Neobundle
-curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > install.sh & sh install.sh & rm install.sh
+curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > install_neo.sh & sh install_neo.sh & rm install_neo.sh
 
 if [ "$(uname)" = "Darwin" ]; then
 	brew install ctags
 elif [ "$(expr substr $(uname -s) 1 5)" = "Linux" ]; then
 	sudo apt-get install exuberant-ctags
 elif [ "$(expr substr $(uname -s) 1 10)" = "MINGW32_NT" ]; then
+	echo "win"
 fi
 
 # Copy dotfiles
